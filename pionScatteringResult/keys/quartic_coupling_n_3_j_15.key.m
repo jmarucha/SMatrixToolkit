12d7b8c8665f5380f99c0e57f98d84aebc8b64c1
Jan K. Marucha <jkmarucha@gmail.com>
2023-10-04 03:08:03 +0200

    more example datapoints

---
+++ pionScatteringResult/keys/quartic_coupling_n_3_j_15.key.m
@@ -0,0 +1,3 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{1, \[Alpha][0, 0, 0], \[Alpha][0, 0, 1], \[Alpha][0, 0, 2], 
+ \[Alpha][0, 0, 3], \[Alpha][0, 1, 1], \[Alpha][0, 1, 2]}
---
+++ pionScatteringResult/keys/quartic_coupling_with_pole_n_1_j_13.key.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{1, g, xi, \[Alpha][0, 0, 0], \[Alpha][0, 0, 1]}
